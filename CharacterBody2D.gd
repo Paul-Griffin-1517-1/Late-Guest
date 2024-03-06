a0e82c4dfd925b945b6fcc12e243c58a6cbfdebd
Paul-Griffin-1517-1 <116929197+Paul-Griffin-1517-1@users.noreply.github.com>
2024-03-05 20:41:27 -0500

    Super simple possession has been implemented

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -3,16 +3,19 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+var is_possessing := false
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var direction : Vector2
+
+var bodies : Array[RigidBody2D]
 func _physics_process(delta):
 	# Add the gravity.
 	#if Input.is_action_just_pressed("Float"):
 		#direction.x = 0
 		#direction.y = 0
+	
 	if Input.is_action_pressed("Float"):
 		
 		direction.y = Input.get_axis("Move Up", "Move Down")
@@ -30,6 +33,8 @@ func _physics_process(delta):
 	else:
 		if not is_on_floor():
 			direction.y += gravity * delta
+		else:
+			direction.y = 0
 		# Handle jump.
 		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 			direction.y = JUMP_VELOCITY
@@ -38,9 +43,32 @@ func _physics_process(delta):
 		direction.x = Input.get_axis("Move Left", "Move Right")
 		if direction.x:
 			velocity.x = direction.x * SPEED
-		#else:
-			#velocity.x = move_toward(velocity.x, 0, SPEED)
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	move_and_slide()
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+	if is_possessing:
+		for i in bodies:
+			pass#i.global_position = i.global_position + direction
+			i.global_position += direction
+			position += direction
+			print(str(i.position))
+	else:
+		move_and_slide()
+func _unhandled_input(event):
+	if event.is_action_pressed("Possess"):
+		is_possessing = !is_possessing
+		print(str(is_possessing))
+		for i in bodies:
+			i.freeze = is_possessing
+
+
+func _on_area_2d_body_entered(body):
+	if body is RigidBody2D:
+		bodies.append(body)
+
+
+func _on_area_2d_body_exited(body):
+	if bodies.has(body):
+		bodies.erase(body)
