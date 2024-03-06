b1cbb26fffdc9601f4f4087288d696901aeb1423
Paul-Griffin-1517-1 <116929197+Paul-Griffin-1517-1@users.noreply.github.com>
2024-03-05 20:16:55 -0500

    Better float mode

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -10,28 +10,36 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var direction : Vector2
 func _physics_process(delta):
 	# Add the gravity.
+	#if Input.is_action_just_pressed("Float"):
+		#direction.x = 0
+		#direction.y = 0
 	if Input.is_action_pressed("Float"):
-		direction.y = lerpf(direction.y,Input.get_axis("Move Up", "Move Down"),1*delta)
+		
+		direction.y = Input.get_axis("Move Up", "Move Down")
+		print(str(direction.y))
 		if direction.y:
-			velocity.y = direction.y * SPEED
-		else:
-			velocity.y = move_toward(velocity.y, 0, SPEED)
-		direction.x = lerpf(direction.x,Input.get_axis("Move Left", "Move Right"),1*delta)
+			velocity.y = lerpf(velocity.y,direction.y * SPEED,2*delta)
+		#else:
+			#velocity.y = move_toward(velocity.y, 0, SPEED)
+		
+		direction.x = Input.get_axis("Move Left", "Move Right")
 		if direction.x:
-			velocity.x = direction.x * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.x = lerpf(velocity.x,direction.x * SPEED,2*delta)
+		#else:
+			#velocity.x = move_toward(velocity.x, 0, SPEED)
 	else:
 		if not is_on_floor():
-			velocity.y += gravity * delta
+			direction.y += gravity * delta
 		# Handle jump.
 		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
+			direction.y = JUMP_VELOCITY
+		velocity.y = direction.y
+
 		direction.x = Input.get_axis("Move Left", "Move Right")
 		if direction.x:
 			velocity.x = direction.x * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+		#else:
+			#velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
