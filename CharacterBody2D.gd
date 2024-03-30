6c4b9869c0d3a727a58846e914709b848cca415b
Paul-Griffin-1517-1 <116929197+Paul-Griffin-1517-1@users.noreply.github.com>
2024-03-30 04:04:25 -0400

    Something resembling a functional prototype

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -1,74 +1,136 @@
 extends CharacterBody2D
 
+@onready var state_chart = $StateChart
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-var is_possessing := false
+var is_possessing := false :
+	set(value):
+		is_possessing = value
+		if is_possessing:
+			state_chart.send_event("possessed")
+		else:
+			state_chart.send_event("not_possessed")
+		
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var direction : Vector2
 
 var bodies : Array[RigidBody2D]
-func _physics_process(delta):
-	# Add the gravity.
-	#if Input.is_action_just_pressed("Float"):
-		#direction.x = 0
-		#direction.y = 0
-	
-	if Input.is_action_pressed("Float"):
-		
-		direction.y = Input.get_axis("Move Up", "Move Down")
-		print(str(direction.y))
-		if direction.y:
-			velocity.y = lerpf(velocity.y,direction.y * SPEED,2*delta)
-		#else:
-			#velocity.y = move_toward(velocity.y, 0, SPEED)
-		
-		direction.x = Input.get_axis("Move Left", "Move Right")
-		if direction.x:
-			velocity.x = lerpf(velocity.x,direction.x * SPEED,2*delta)
-		#else:
-			#velocity.x = move_toward(velocity.x, 0, SPEED)
-	else:
-		if not is_on_floor():
-			direction.y += gravity * delta
-		else:
-			direction.y = 0
-		# Handle jump.
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-			direction.y = JUMP_VELOCITY
-		velocity.y = direction.y
 
-		direction.x = Input.get_axis("Move Left", "Move Right")
-		if direction.x:
-			velocity.x = direction.x * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+var is_floating := false
 
-		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom gameplay actions.
+func pp_float_movement(delta):
+	is_floating = true
+	set_collision_mask_value(1, false)
+	direction.y = Input.get_axis("Move Up", "Move Down")
+	velocity.y = lerpf(velocity.y,direction.y * SPEED,2*delta)
 	if is_possessing:
-		for i in bodies:
-			pass#i.global_position = i.global_position + direction
-			i.global_position += direction
-			position += direction
-			print(str(i.position))
+		velocity.y += (gravity * delta)*.2
+		if bodies.size() > 0:
+			velocity.y *= bodies[0].mass
+	direction.x = Input.get_axis("Move Left", "Move Right")
+	velocity.x = lerpf(velocity.x,direction.x * SPEED,2*delta)
+	pp_move_and_slide()
+
+func pp_normal_movement(delta):
+	is_floating = false
+	set_collision_mask_value(1, true)
+	if not is_on_floor():
+		direction.y += gravity * delta
 	else:
-		move_and_slide()
+		direction.y = 0
+	# Handle jump.
+	if Input.is_action_just_pressed("Jump"):
+		if is_possessing and bodies.size() > 0:
+			#print(str(abs(bodies[0].linear_velocity.y)))
+			if abs(bodies[0].linear_velocity.y) == 0:
+				direction.y *= JUMP_VELOCITY
+			#direction.y += (bodies[0].mass)
+		else:
+			if is_on_floor():
+				direction.y = JUMP_VELOCITY
+	velocity.y = direction.y
+
+	direction.x = Input.get_axis("Move Left", "Move Right")
+	if direction.x:
+		velocity.x = direction.x * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if velocity.x:
+		if velocity.x > 1:
+			sprite.scale.x = -0.168
+		else:
+			sprite.scale.x = 0.168
+	pp_move_and_slide()
+
+
+
+func pp_move_and_slide():
+	#if is_possessing:
+		#for i in bodies:
+			#i.global_position += direction
+			#position += direction
+#
+		#sprite.visible = false
+	#else:
+	move_and_slide()
+		#sprite.visible = true
+func pp_possess(delta):
+	#for i in bodies:
+		#i.position = position
+	pass
+
+
+
+func _physics_process(delta):
+	if Input.is_action_pressed("Float"):
+		state_chart.send_event("float_pressed")
+	else:
+		state_chart.send_event("float_not_pressed")
+	
 func _unhandled_input(event):
-	if event.is_action_pressed("Possess"):
+	if event.is_action_pressed("Possess") and bodies.size() > 0:
 		is_possessing = !is_possessing
-		print(str(is_possessing))
 		for i in bodies:
 			i.freeze = is_possessing
 
 
-func _on_area_2d_body_entered(body):
-	if body is RigidBody2D:
-		bodies.append(body)
+func _on_area_2d_area_entered(area):
+	#if area:
+	bodies.append(area.get_parent())
 
 
-func _on_area_2d_body_exited(body):
-	if bodies.has(body):
-		bodies.erase(body)
+func _on_area_2d_area_exited(area):
+	if bodies.has(area.get_parent()):
+		bodies.erase(area.get_parent())
+@onready var sprite := $Sprite2D
+
+func _ready():
+	sprite.play("default")
+
+@onready var sprite_2d_2 := $Sprite2D2
+
+func _on_normal_state_entered():
+	sprite.visible = true
+	sprite_2d_2.visible = false
+	for i in bodies:
+		i.global_position = global_position
+		#global_position.y += 10
+		#i.global_position.x += 12
+		i.visible = true
+		i.get_child(1).disabled = false
+
+
+func _on_possessing_state_state_entered():
+	for i in bodies:
+		#i.global_position += direction
+		#position += direction
+		i.visible = false
+		i.get_child(1).disabled = true
+	sprite.visible = false
+	sprite_2d_2.visible = true
+	sprite_2d_2.texture = bodies[0].get_child(0).texture
+	$GPUParticles2D.emitting = true
