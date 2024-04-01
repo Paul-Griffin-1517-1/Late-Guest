e0e2e85d33c4c2d50d38b8b455ca8143a0b92787
Eisegesis <116929197+Paul-Griffin-1517-1@users.noreply.github.com>
2024-04-01 16:52:39 -0400

    Added conditions to ConditionZone

--- ConditionZone.gd
+++ ConditionZone.gd
@@ -2,14 +2,25 @@ extends Area2D
 
 
 @export_enum("WIN","LOSE") var zone_type = "WIN"
-
+@export_enum("ALL","WHEN_POSSESSED","WHEN_NORMAL") var condition = "ALL"
 
 
 func _on_body_entered(body:CharacterBody2D):
 	print("thing")
 	if body.is_in_group("Player"):
-		match zone_type:
-			"WIN":
-				Global.win()
-			"LOSE":
-				Global.lose()
+		match condition:
+			"ALL":
+				destinator()
+			"WHEN_POSSESSED":
+				if body.is_possessing:
+					destinator()
+			"WHEN_NORMAL":
+				if !body.is_possessing:
+					destinator()
+
+func destinator():
+	match zone_type:
+		"WIN":
+			Global.win()
+		"LOSE":
+			Global.lose()
