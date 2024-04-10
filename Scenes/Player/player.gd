fd56f4c06de6bf8dcc8797f7caca2cb5bf54fa5c
Eisegesis <116929197+Paul-Griffin-1517-1@users.noreply.github.com>
2024-04-10 15:26:10 -0400

    Building the Bones

---
+++ Scenes/Player/player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+var phase_entry_point : Vector2
+var exit_point : Area2D = null
+#var possessable_object : Node2D
+var possessable_objects_in_range : Array[Node2D]
+var actionables_in_range : Area2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
