fef599f52a9dc58545361b8a3dd8a04148e0f4e2
Eisegesis <116929197+Paul-Griffin-1517-1@users.noreply.github.com>
2024-04-10 14:08:33 -0400

    Deleted All of the Things

--- Global.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-
-func win():
-	get_tree().change_scene_to_file("res://victory.tscn")
-func lose():
-	get_tree().change_scene_to_file("res://defeat.tscn")
--- README.md
+++
@@ -1,6 +0,0 @@
-# Late Guest
- 
-Uses: Godot 4.2.1 Stable
-Controls
-WASD to move
-Left Click or Shift to float
