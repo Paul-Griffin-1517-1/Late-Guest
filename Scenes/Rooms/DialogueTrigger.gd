7fef8201dda8eca219d468c5985661b85bad54df
Eisegesis <116929197+Paul-Griffin-1517-1@users.noreply.github.com>
2024-04-10 16:21:52 -0400

    Dialogue Works

---
+++ Scenes/Rooms/DialogueTrigger.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+@export_file("*.dialogue") var _dialogue_resource := "res://Dialogue/DialogueFile.dialogue"
+var dialogue_resource
+
+var can_action := false
+
+func _ready():
+	dialogue_resource = load(_dialogue_resource)
+
+func _unhandled_input(event):
+	## TODO: This is temp code just to make sure the action() method works.
+	if event.is_action_pressed("Jump"):
+		can_action = true
+		action()
+
+func action():
+	if can_action:
+		print("Talk")
+		DialogueManager.show_dialogue_balloon_scene("res://Dialogue/balloon.tscn",dialogue_resource, "intro", [self])
---
+++ Scenes/Rooms/room_base.gd
@@ -0,0 +1,11 @@
+extends Node2D
+@onready var spawn_points_holder = $SpawnPointsHolder
+@onready var exit_points_holder = $ExitPointsHolder
+
+
+var spawn_points : Array[Marker2D]
+func _ready():
+	for i in spawn_points_holder.get_children():
+		if i is Marker2D:
+			spawn_points.append(i)
+
